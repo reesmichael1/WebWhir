6269705247eaffaebc3aa670d711425f59f6929e
Michael Rees <rees.michael1@gmail.com>
2013-02-25 21:40:28 -0700

    Continuing prototype development. OpenWeb can now read a file, which will be necessary to read HTML documents as development continues.

--- src/htmlreader.cpp
+++ src/htmlreader.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <fstream>
 #include "htmlreader.h"
 
 HTMLReader::HTMLReader()
 {
 }
 
-void HTMLReader::parseDocument(FILE *HTMLDocument)
+void HTMLReader::parseDocument(char HTMLFilepath[])
 {
-    std::cout << "Now entering the parser." << std::endl;
+    try
+    {
+        std::ifstream HTMLDocument(HTMLFilepath);
+        if (!HTMLDocument.is_open())
+        {
+            throw 0;
+        }
+        else
+        {
+            std::cout << "Now entering the parser." << std::endl;
+        }
+    }
+
+    catch (int i)
+    {
+        if (i == 0)
+        {
+            std::cerr << "Error: Could not open the document." << std::endl;
+        }
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,19 @@
 int main(int argc, char* argv[])
 {
 
-    FILE *HTMLDocument;
-    HTMLDocument = fopen(argv[1], "r");
+    if (argc > 1)
+    {
+        HTMLReader::parseDocument(argv[1]);
+    }
 
-    HTMLReader::parseDocument(HTMLDocument);
+    else
+    {
+        std::cout << "Please enter the filepath of the HTML Document." << std::endl;
+        char HTMLFilepath[256];
+        std::cin >> HTMLFilepath;
+
+        HTMLReader::parseDocument(HTMLFilepath);
+    }
 
     return 0;
 }
