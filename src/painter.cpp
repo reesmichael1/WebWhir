85a0ee4a8469d250724a12c96948380c53a46c78
Michael Rees <rees.michael1@gmail.com>
2013-03-12 20:02:57 -0600

    Continuing work on fonts in SFML.

--- src/painter.cpp
+++ src/painter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "SFML/System.hpp"
 #include "SFML/Graphics/Sprite.hpp"
 #include "SFML/Graphics/Text.hpp"
 #include "SFML/Graphics.hpp"
@@ -9,39 +10,30 @@ Painter::Painter(RenderNode *node)
 {
 
     sf::RenderWindow window(sf::VideoMode(800, 600, 32), "OpenWeb");
-
-
+    paintNode(node, &window);
 }
 
-void Painter::paintNode(RenderNode *node)
+
+void Painter::paintNode(RenderNode *node, sf::RenderWindow *window)
 {
     try
     {
-        while (window.isOpen())
+
+        sf::Text text(node->getText());
+        while (window->isOpen())
         {
-            sf::Font font;
-
-            if (!font.loadFromFile("fonts/Timeless-Bold.ttf"))
-            {
-                throw "Error: Could not load the font.";
-            }
-
-            sf::Text text;
-            text.setColor(sf::Color(255,255,255));
-            text.setString(node->getText());
-            text.setCharacterSize(15);
-            window.draw(text);
-            window.display();
+            window->clear();
+            window->draw(text);
+            window->display();
 
             sf::Event event;
-            while (window.pollEvent(event))
+            while (window->pollEvent(event))
             {
                 if (sf::Event::Closed == event.type)
                 {
-                    window.close();
+                    window->close();
                 }
             }
-            window.clear();
         }
     }
 
--- src/painter.h
+++ src/painter.h
@@ -1,13 +1,17 @@
 #ifndef PAINTER_H
 #define PAINTER_H
 
+#include "SFML/Graphics.hpp"
+#include "SFML/Graphics/Text.hpp"
 #include "rendernode.h"
 
 class Painter
 {
 public:
     Painter(RenderNode *node);
-    static void paintNode(RenderNode *node);
+    static void paintNode(RenderNode *node, sf::RenderWindow *window);
+
+private:
 
 };
 
