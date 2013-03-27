759b805bbab31daf2f1cd8a7ebb9c1f315dec572
Michael Rees <rees.michael1@gmail.com>
2013-03-27 09:10:18 -0600

    Commit to merge changes.

--- src/painter.cpp
+++ src/painter.cpp
@@ -57,7 +57,7 @@ void Painter::paintNode(RenderNode *node, sf::RenderWindow *window)
         sf::Text text;
 
 
-        text.setString(parseTextToLines(node->getText(), window->getSize().x));
+        text.setString(parseTextToLines(node->getText(), 2000));
         text.setCharacterSize(15);
 
         sf::Font font;
