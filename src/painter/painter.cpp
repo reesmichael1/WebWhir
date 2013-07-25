595771937227096944fa7e6fe8306bff833c293f
reesmichael1 <reesmichael1@gmail.com>
2013-07-25 10:22:45 -0600

    Explain why the Painter class exists.

--- src/painter/painter.cpp
+++ src/painter/painter.cpp
@@ -2,6 +2,13 @@
 
 #include <QPainter>
 
+//Currently, this is a useless class. As part of the refactoring,
+//I will make this a custom widget and set it as part of the layout
+//in the main window. This means that I can set the paint event for
+//the painter to match the paint event of the main window. Then, I
+//will be able to set this widget as the widget of a scroll area,
+//which should allow me to implement scrolling.
+
 Painter::Painter(MainWindow *window)
 {
     mainWindow = window;
